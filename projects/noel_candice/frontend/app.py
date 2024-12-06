9fb112696e19d0deff772f8bfe684f37c9eecd17
jjmouss <joachim.moussalli@gmail.com>
2024-12-06 20:57:52 +0100

    login page

--- projects/noel_candice/frontend/app.py
+++ projects/noel_candice/frontend/app.py
@@ -1,14 +1,46 @@
 import streamlit as st
+import yaml
+from yaml.loader import SafeLoader
+import streamlit_authenticator as stauth
+from streamlit_authenticator import Authenticate
+from background import set_bg_hack
 
-# Create a simple web app with Streamlit
-st.title("Name Input App")
+with open("crendentials.yaml") as file:
+    config = yaml.load(file, Loader=SafeLoader)
 
-# Text input for user's name
-name = st.text_input("Enter your name:")
+authenticator = Authenticate(
+    config["credentials"],
+    config["cookie"]["name"],
+    config["cookie"]["key"],
+    config["cookie"]["expiry_days"],
+)
 
-# Button to submit
-if st.button("Submit"):
-    if name:
-        st.write(f"Hello, {name}!")
-    else:
-        st.write("Please enter your name.")
+set_bg_hack("background/noel_blanc.jpg")
+st.write("Pour un Joyeux Noel")
+try:
+    authenticator.login()
+except Exception as e:
+    st.error(e)
+
+
+if st.session_state["authentication_status"]:
+    authenticator.logout()
+    st.write(f'Welcome *{st.session_state["name"]}*')
+    st.title('Some content')
+elif st.session_state['authentication_status'] is False:
+    st.error('Username/password is incorrect')
+elif st.session_state['authentication_status'] is None:
+    st.warning('Please enter your username and password')
+
+# # Create a simple web app with Streamlit
+# st.title("Name Input App")
+
+# # Text input for user's name
+# name = st.text_input("Enter your name:")
+
+# # Button to submit
+# if st.button("Submit"):
+#     if name:
+#         st.write(f"Hello, {name}!")
+#     else:
+#         st.write("Please enter your name.")
---
+++ projects/noel_candice/frontend/background.py
@@ -0,0 +1,25 @@
+import base64
+import streamlit as st
+
+def set_bg_hack(main_bg):
+    '''
+    A function to unpack an image from root folder and set as bg.
+ 
+    Returns
+    -------
+    The background.
+    '''
+    # set bg name
+    main_bg_ext = "jpg"
+        
+    st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background: url(data:background/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()});
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
