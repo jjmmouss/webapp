d3a3514dd3a8e0c13792d9af4097afed1206ae3c
jjmouss <joachim.moussalli@gmail.com>
2024-12-06 23:56:28 +0100

    add todos

--- projects/noel_candice/frontend/pages/enigme_1.py
+++ projects/noel_candice/frontend/pages/enigme_1.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
+# TODO: Add return button to main_page
+# TODO: Add saving credentials when solved
+# TODO: Add gift when solved
 if st.session_state["authentication_status"]:
     st.write("Bienvenue dans la première enigme")
 else:
